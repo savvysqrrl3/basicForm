7f72e0a2b5b98bbedd977d01cdfe250084933edb
April Cound <musicwithapril@gmail.com>
2018-09-27 16:24:59 -0700

    fixed the setting of state and handleChange for each form field, added padding to text input fields

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,13 +4,18 @@ import styles from './Form.scss';
 class Form extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            email: '',
+            password: '',
+            verify: '',
+            terms: false,
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    handleChange({target}) {
+        this.setState({[target.name]: target.value});
     }
     
     handleSubmit(event) {
@@ -23,16 +28,41 @@ class Form extends React.Component {
         return(
             <form onSubmit={this.handleSubmit}>
                 <p className={styles.inputLabel}>Account Email</p>
-                <input type="text" name='email' className={styles.inputField} value={this.state.value} onChange={this.handleChange} />
+                <input 
+                    type="text" 
+                    name='email' className={styles.inputField} value={this.state.email} 
+                    onChange={this.handleChange} 
+                />
                 <p className={styles.inputLabel}>Password</p>
-                <input type="text" name='password' className={styles.inputField} value={this.state.value} onChange={this.handleChange} />
+                <input 
+                    type="text" 
+                    name='password' 
+                    className={styles.inputField} 
+                    value={this.state.password} 
+                    onChange={this.handleChange} 
+                />
                 <p className={styles.inputLabel}>Verify Password</p>
-                <input type="text" name='password-verify' className={styles.inputField} value={this.state.value} onChange={this.handleChange} />
+                <input 
+                    type="text" 
+                    name='verify' 
+                    className={styles.inputField} 
+                    value={this.state.verify} 
+                    onChange={this.handleChange} 
+                />
                 <p className={styles.inputLabel}>
-                    <input type="checkbox" name="terms" className={styles.checkBox} checked="" />
+                    <input 
+                        type="checkbox" 
+                        name="terms" 
+                        className={styles.checkBox} 
+                        value={this.state.terms} 
+                    />
                     Terms and Conditions
                 </p>
-                <input type="submit" className={styles.buttonLarge} value="Sign In" />
+                <input 
+                    type="submit" 
+                    className={styles.buttonLarge} 
+                    value="Sign In" 
+                />
             </form>
         );
     }
